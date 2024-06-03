0d9141f32d0c6dec61ddea343089369b04b5c1eb
LeeWest89 <lee.west@atlasschool.com>
2024-06-03 15:37:47 -0500

    Header is almost finished need to add links then just javascript

--- scripts.js
+++ scripts.js
@@ -2,7 +2,24 @@ $(document).ready(function() {
 
     document.getElementById('navbarDropdown').addEventListener('click', function() {
         let icon = document.getElementById('icon');
-        icon.classList.toggle('rotate-90');
+        icon.classList.toggle('rotate');
+
+    });
+
+    document.getElementById('svg-link').addEventListener('mouseover', function() {
+        document.getElementById('Dribbble-Light-Preview').style.fill = 'aqua';
+    });
+    
+    document.getElementById('svg-link').addEventListener('mouseout', function() {
+        document.getElementById('Dribbble-Light-Preview').style.fill = '';
+    });
+
+    document.getElementById('li-link').addEventListener('mouseover', function() {
+        document.getElementById('path').style.fill = 'aqua';
+    });
+
+    document.getElementById('li-link').addEventListener('mouseout', function() {
+        document.getElementById('path').style.fill = '#ffffff';
     });
 
 });

16dcd479b2a611f072c7303bb54d7748a8ba4afa
LeeWest89 <lee.west@atlasschool.com>
2024-06-03 14:09:14 -0500

    Have almost everything ready now working on js and github and linkedIn links

--- scripts.js
+++ scripts.js
@@ -1,1 +1,8 @@
-#include
+$(document).ready(function() {
+
+    document.getElementById('navbarDropdown').addEventListener('click', function() {
+        let icon = document.getElementById('icon');
+        icon.classList.toggle('rotate-90');
+    });
+
+});
